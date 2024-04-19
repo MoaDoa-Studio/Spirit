48d90c0517bef8703c5422639e5b8b8bdc783f8a
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2024-04-19 23:07:43 +0900

    울타리 시스템 로직 구현
    -

---
+++ Project_Spirit/Assets/Scripts/CradleManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CradleManager : MonoBehaviour
+{
+    private float[] elementGrowth = { 0f, 0f, 0f, 0f };
+    private float cradleGrowth = 0f;
+
+    private float declineCount = 4f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 4원소 성장 게이지 관리 변수.
+    // 정령왕 성장 게이지 변수.            
+
+    // 정령왕 레벨업 함수.
+
+    // 정령이 요람에 부딪혔을 경우 호출되는 함수 설계.
+
+    // 하단 UI 갱신 함수.
+    // 갱신 목록, 각 원소별 성장 속도 게이지, 정령 성장 속도 마크, 정령왕의 성장 게이지, 정령왕 이미지
+    // 서서히 올라가는 게이지는 코루틴 이용해서 설계.
+
+    // 정령왕의 성장 속도 계산 및 판정 함수.
+
+    // 원소 조화, 부조화 판정 함수.
+
+    // 게임 오버 함수.
+
+    
+}
