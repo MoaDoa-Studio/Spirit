a11f0cb1f866c03aeedb83c9f3ad38cb0ffbf386
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2024-04-22 21:56:24 +0900

    요람 시스템 개발 완료
    - 요람 레벨 업 시스템 구현.
    - 요람 성장 경험치 획득 및 소실 구현.
    - 각 원소별 게이지 사이징만 결정되면 끝.

--- Project_Spirit/Assets/Scripts/CradleManager.cs
+++ Project_Spirit/Assets/Scripts/CradleManager.cs
@@ -9,31 +9,38 @@ public class CradleManager : MonoBehaviour
 {
     [Header("오브젝트")]
     [SerializeField]
-    private GameObject cradleState;    
+    private GameObject cradle;    
     [SerializeField]
     private GameObject cradleGrowthRate;    
     [SerializeField]
     private GameObject[] elementSlider;
-
+    [SerializeField]
+    private GameObject cradleSlider;
+    
     [Header("스프라이트")]
     [SerializeField]
     private Sprite[] cradleSprite;
     [SerializeField]
     private Sprite[] cradleGrowthRateSprite;
-
-    private int cradleLevel = 0;        
-
-    // 원소 값 계산용 변수.
-    // 불, 물, 땅, 공기.
+    
+    // 원소 관련 변수.
     Queue<Tuple<int, DateTime>>[] elementQueue = { 
         new Queue<Tuple<int, DateTime>>(), 
-        new Queue<Tuple<int, DateTime>>() , 
-        new Queue<Tuple<int, DateTime>>() , 
+        new Queue<Tuple<int, DateTime>>(), 
+        new Queue<Tuple<int, DateTime>>(), 
         new Queue<Tuple<int, DateTime>>() 
     };
-    public float[] elementAvarage = { 0, 0, 0, 0 };
+    public float[] elementAverage = { 0, 0, 0, 0 };
     public int[] elementSum = { 0, 0, 0, 0 };
-    TimeSpan span = TimeSpan.FromSeconds(10); // 인게임 내 1일의 실제 시간 값.
+    TimeSpan span = TimeSpan.FromSeconds(10);
+
+    // 요람 관련 변수.
+    private int cradleLevel = 0;
+    private int cradleGrowth = 0;
+    private int cradleGrowthState = 0;
+    private int[] cradleGrowthValue = { 50, 25, 10, -40 };
+    private float cradleGrowthTime = 0f;
+    private float cradleGrowthCooldown = 10f;
 
     // For Debug.
     [SerializeField]
@@ -43,49 +50,87 @@ public class CradleManager : MonoBehaviour
     void Start()
     {
         // For Debug.
-        DebuggingBtn.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Fire", 10));
-        DebuggingBtn.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Water", 10));
-        DebuggingBtn.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Ground", 10));
-        DebuggingBtn.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Air", 10));
-        DebuggingBtn.transform.GetChild(4).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Fire", 50));
-        DebuggingBtn.transform.GetChild(5).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Water", 20));
-        DebuggingBtn.transform.GetChild(6).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Ground", 20));
-        DebuggingBtn.transform.GetChild(7).GetComponent<Button>().onClick.AddListener(() => AddSpiritToCradle("Air", 20));
+        DebuggingBtn.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => AddElement("Fire", 10));
+        DebuggingBtn.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(() => AddElement("Water", 10));
+        DebuggingBtn.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() => AddElement("Ground", 10));
+        DebuggingBtn.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => AddElement("Air", 10));
+        DebuggingBtn.transform.GetChild(4).GetComponent<Button>().onClick.AddListener(() => AddElement("Fire", 30));
+        DebuggingBtn.transform.GetChild(5).GetComponent<Button>().onClick.AddListener(() => AddElement("Water", 20));
+        DebuggingBtn.transform.GetChild(6).GetComponent<Button>().onClick.AddListener(() => AddElement("Ground", 15));
+        DebuggingBtn.transform.GetChild(7).GetComponent<Button>().onClick.AddListener(() => AddElement("Air", 40));
+        //
     }
 
     // Update is called once per frame    
     void Update()
     {
         RemoveExpiredElement();
-        CalculateElementAvarage();
-        UpdateCradleUI();
+        CalculateElementAverage();
+        AddCradleGrowth();
 
+        UpdateCradleUI();        
+    
         // For Debug.
         SetDebuggingText();
+        //
     }
 
     // 4원소 성장 게이지 관리 변수.
     // 정령왕 성장 게이지 변수.            
+    void AddCradleGrowth()
+    {
+        cradleGrowthTime += Time.deltaTime;
+        if (cradleGrowthTime > cradleGrowthCooldown)
+        {
+            cradleGrowth += cradleGrowthValue[cradleGrowthState];
+
+            if (cradleGrowth > 100)
+                ToNextCradle();
+            else if (cradleGrowth < 100)
+            {
+                // 게임 오버.
+            }            
+            
+            SetCradleGrowthSlider();
+            cradleGrowthTime = 0f;
+        }
+    }
+
+    void SetCradleGrowthSlider()
+    {
+        if (cradleGrowth < 0)
+        {
+            cradleSlider.GetComponent<Slider>().value = cradleGrowth * (-1);
+            cradleSlider.transform.Find("Fill Area/Fill").GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            cradleSlider.GetComponent<Slider>().value = cradleGrowth;
+            cradleSlider.transform.Find("Fill Area/Fill").GetComponent<Image>().color = Color.green;
+        }
+    }
 
     // 정령왕 레벨업 함수.
     public void ToNextCradle()
     {
         cradleLevel++;
-        if (cradleLevel >= 8)
+        cradleGrowth = 0;
+        if (cradleLevel < 8)
+            cradle.GetComponent<Image>().sprite = cradleSprite[cradleLevel];
+        else
         {
             // 게임 클리어 판정.
-        }        
+        }
     }
 
     // 정령이 요람에 부딪혔을 경우 호출되는 함수 설계.
-    public void AddSpiritToCradle(string name, int val)
+    public void AddElement(string name, int val)
     {
         switch (name)
         {
             case "Fire":                
-                elementQueue[0].Enqueue(new Tuple<int, DateTime>(val, DateTime.Now));
-                Debug.Log(DateTime.Now);
-                elementSum[0] += val;                
+                elementQueue[0].Enqueue(new Tuple<int, DateTime>(val, DateTime.Now));                
+                elementSum[0] += val;
                 break;
             case "Water":
                 elementQueue[1].Enqueue(new Tuple<int, DateTime>(val, DateTime.Now));
@@ -110,23 +155,22 @@ public class CradleManager : MonoBehaviour
             {
                 // Temp. 현재 시간과 가장 과거의 원소의 진입 시간이 10초 이내로 차이 날 경우 Break;
                 if (DateTime.Now - elementQueue[i].Peek().Item2 < span)
-                    break;
-                Debug.Log("파괴 " + DateTime.Now);
-                elementSum[i] -= elementQueue[i].Peek().Item1;
-                Debug.Log(i + "원소 " + elementQueue[i].Peek().Item1);
+                    break;                
+                
+                elementSum[i] -= elementQueue[i].Peek().Item1;                
                 elementQueue[i].Dequeue();
             }            
         }
     }
 
-    public void CalculateElementAvarage()
+    public void CalculateElementAverage()
     {
         for (int i = 0; i < 4; i++)
         {
             if (elementQueue[i].Count == 0)            
-                elementAvarage[i] = 0f;            
+                elementAverage[i] = 0f;            
             else
-                elementAvarage[i] = elementSum[i] / elementQueue[i].Count;
+                elementAverage[i] = elementSum[i] / elementQueue[i].Count;
         }
     }
     // 하단 UI 갱신 함수.
@@ -134,72 +178,98 @@ public class CradleManager : MonoBehaviour
     // 서서히 올라가는 게이지는 코루틴 이용해서 설계.
     public void UpdateCradleUI()
     {
-        if (cradleLevel < 8)
-            cradleState.GetComponent<Image>().sprite = cradleSprite[cradleLevel];
-
-        ShowSpiritSlider(); // 정령 값 슬라이더.
-        CalculateCradleGrowthRate(); // 요람 성장 속도 계산.
+        // 요람 성장 상태 갱신.
+        SetCradleGrowthState(GetCradleGrowthRate());
+        
+        // 원소 기여 슬라이더 갱신.
+        SetElementSliderColor();
+        SetElementSliderSize();        
     }
 
     // 원소별 성장 속도 게이지 표시
-    public void ShowSpiritSlider()
+    public void SetElementSliderSize()
     {
-        float totalElementAverage = GetTotalElementAverage();
+        float totalElementAverage = GetTotalElementAverage();       
+        float offset = 100f;
         for (int i = 0; i < 4; i++)
         {
             RectTransform result = elementSlider[i].transform.GetChild(0).GetComponent<RectTransform>();
-            float val = elementAvarage[i] - totalElementAverage;
-            
+            float val = (elementAverage[i] - totalElementAverage) / totalElementAverage * offset;
+
             float x_pos = val;
-            float width = val > 0 ? val * 2 : val * (-2);
+            float width = 0f;
+            if (val > offset)
+            {
+                x_pos = offset;
+                width = offset * 2;
+            }
+            else if (val < offset * (-1))
+            {
+                x_pos = offset * (-1);
+                width = offset * (-2);
+            }
+            else
+                width = val > 0 ? val * 2 : val * (-2);
+
             result.sizeDelta = new Vector2(width, result.rect.height);
             result.anchoredPosition = new Vector2(x_pos, 0);
         }
     }
 
     // 정령왕의 성장 속도 계산 및 판정 함수.
-    public void CalculateCradleGrowthRate()
+    public int GetCradleGrowthRate()
     {
         float totalElementAverage = GetTotalElementAverage();
         int result = 0;
-
         for (int i = 0; i < 4; i++)
         {
-            if (totalElementAverage * 0.9f <= elementAvarage[i] && totalElementAverage * 1.1f >= elementAvarage[i])
-            {
-                result += 1;
-                elementSlider[i].transform.GetChild(0).GetComponent<Image>().color = Color.green;
-            }
-            else if (totalElementAverage * 0.65f <= elementAvarage[i] && totalElementAverage * 1.35f >= elementAvarage[i])
-            {
-                result += 2;
-                elementSlider[i].transform.GetChild(0).GetComponent<Image>().color = Color.yellow;
-            }
-            else
-            {
-                result += 3;
-                elementSlider[i].transform.GetChild(0).GetComponent<Image>().color = Color.red;
-            }
-        }
+            if (totalElementAverage * 0.9f <= elementAverage[i] && totalElementAverage * 1.1f >= elementAverage[i])            
+                result += 1;                            
+            else if (totalElementAverage * 0.65f <= elementAverage[i] && totalElementAverage * 1.35f >= elementAverage[i])            
+                result += 2;                
+            else            
+                result += 3;                
+        }        
+        return result;
+    }
 
-        if (result <= 4)
-        {        
+    void SetElementSliderColor()
+    {
+        float totalElementAverage = GetTotalElementAverage();        
+        for (int i = 0; i < 4; i++)
+        {
+            if (totalElementAverage * 0.9f <= elementAverage[i] && totalElementAverage * 1.1f >= elementAverage[i])            
+                elementSlider[i].transform.GetChild(0).GetComponent<Image>().color = Color.green;            
+            else if (totalElementAverage * 0.65f <= elementAverage[i] && totalElementAverage * 1.35f >= elementAverage[i])            
+                elementSlider[i].transform.GetChild(0).GetComponent<Image>().color = Color.yellow;            
+            else            
+                elementSlider[i].transform.GetChild(0).GetComponent<Image>().color = Color.red;            
+        }
+    }
+    void SetCradleGrowthState(int val)
+    {
+        if (val <= 4)
+        {
+            cradleGrowthState = 0;
             cradleGrowthRate.GetComponent<Image>().sprite = cradleGrowthRateSprite[0];
         }
-        else if (result <= 6)
-        {            
+        else if (val <= 6)
+        {
+            cradleGrowthState = 1;
             cradleGrowthRate.GetComponent<Image>().sprite = cradleGrowthRateSprite[1];
         }
-        else if (result <= 8)
-        {         
+        else if (val <= 8)
+        {
+            cradleGrowthState = 2;
             cradleGrowthRate.GetComponent<Image>().sprite = cradleGrowthRateSprite[2];
         }
         else
-        {            
+        {
+            cradleGrowthState = 3;
             cradleGrowthRate.GetComponent<Image>().sprite = cradleGrowthRateSprite[3];
         }
     }
-
+    
     float GetTotalElementAverage()
     {
         int sum = 0, count = 0;
@@ -220,9 +290,9 @@ public class CradleManager : MonoBehaviour
     void SetDebuggingText()
     {
         DebuggingText[0].text = "Total : " + GetTotalElementAverage().ToString();
-        DebuggingText[1].text = "Fire : " + elementAvarage[0].ToString();
-        DebuggingText[2].text = "Water : " + elementAvarage[1].ToString();
-        DebuggingText[3].text = "Ground : " + elementAvarage[2].ToString();
-        DebuggingText[4].text = "Air : " + elementAvarage[3].ToString();
+        DebuggingText[1].text = "Fire : " + elementAverage[0].ToString();
+        DebuggingText[2].text = "Water : " + elementAverage[1].ToString();
+        DebuggingText[3].text = "Ground : " + elementAverage[2].ToString();
+        DebuggingText[4].text = "Air : " + elementAverage[3].ToString();
     }
 }
