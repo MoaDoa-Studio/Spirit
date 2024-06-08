2d9004bd9d98cdebc831cf0f508a1002d419c93b
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2024-06-08 17:00:03 +0900

    퀘스트 기능 구현
    -

--- Project_Spirit/Assets/Scripts/Quest/Quest.cs
+++ Project_Spirit/Assets/Scripts/Quest/Quest.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
-public class Quest : MonoBehaviour, IPointerEnterHandler
+using TMPro;
+public class Quest : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    // 데이터 받아오기 전까진 하드코딩.
     public int QuestID;
     public string QuestName;
     public string QuestBody;
@@ -18,11 +21,46 @@ public class Quest : MonoBehaviour, IPointerEnterHandler
     public float ConditionStandard;
 
     public int CurrentConditionAchieve;
+    [SerializeField]
+    private TextMeshProUGUI TimeText;
 
-    private readonly int[] QuestPrefabSize = new int[2] { 50, 100 };
-    private readonly int[] QuestNamePos = new int[2] { 0, 25 };
+    private readonly float[] QuestPrefabHeight = new float[] { 50, 100 };
+    private readonly float[] QuestNamePos = new float[] { 0, 25 };
+
+    private bool isExpanded;
+    private bool isCleared;
+    private float time;
+    
     // For Debug.
-    public Quest(int _id, string _name, string _body, string _conditionText,
+    public void Start()
+    {
+        CurrentConditionAchieve = 0;
+        isExpanded = false;
+        isCleared = false;
+        time = 0f;
+        TimeText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();        
+
+    }
+
+    public void Update()
+    {
+        if (isCleared == false)
+        {
+            time += Time.deltaTime;
+            if (time >= 1f)
+            {
+                time = 0f;
+                SetTime();
+                if (QuestClearTime <= 0)
+                    FailQuest();
+                else
+                    QuestClearTime -= 1;
+            }
+        }
+    }
+
+    // Todo. 데이터 테이블 불러오는 기능 구현되면 데이터 ID 값을 통해 모든 정보 불러올 수 있도록 수정할거.
+    public void SetQuest(int _id, string _name, string _body, string _conditionText,
         int _condition, int _clearCondition, int _clearTime, int _disadvantage)
     {
         QuestID = _id;
@@ -33,32 +71,83 @@ public class Quest : MonoBehaviour, IPointerEnterHandler
         QuestClearCondition = _clearCondition;
         QuestClearTime = _clearTime;
         QuestDisadvantage = _disadvantage;
-    }
 
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _name;
+        transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = _body;
+        transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = _conditionText;
+        SetTime();
+    }
     public void SetCondition(int _cID, int _cTarget, float _cStandard)
     {
         ConditionID = _cID;
         ConditionTarget = _cTarget;
         ConditionStandard = _cStandard;
     }
+    public void SetTime()
+    {
+        int min = QuestClearTime / 60;
+        int sec = QuestClearTime % 60;        
+        string _min = min.ToString();
+        string _sec = sec.ToString();        
+        if (_min.Length < 2)        
+            _min = "0" + _min;
+        if (_sec.Length < 2)
+            _sec = "0" + _sec;
 
+        TimeText.text = _min + ":" + _sec;
+    }
     public bool CheckClear()
     {
         if (CurrentConditionAchieve >= ConditionStandard)
             return true;
         return false;
     }
-    public void Start()
-    {
-        CurrentConditionAchieve = 0;
-    }
 
-    public delegate void OnPointerHandler();
-    public OnPointerHandler pHandler;
-    public void OnPointerEnter(PointerEventData eventData)
+    public void ClearQuest()
     {
-        pHandler?.Invoke();
+        // 보상 지급 로직
+
+        //
+        isCleared = true;
+        SetQuestSize(1);
+        transform.GetComponent<Image>().color = Color.green;
+        Destroy(this.gameObject, 1f);        
+    }
+    public void FailQuest()
+    {
+        // 디스어드밴티지
+
+        //
+        SetQuestSize(1);
+        transform.GetComponent<Image>().color = Color.red;
+        Destroy(this.gameObject, 1f);
     }    
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {        
+        SetQuestSize(1);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {        
+        SetQuestSize(0);        
+    }
+    void SetQuestSize(int num)
+    {        
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(300, QuestPrefabHeight[num]);
+        transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(-49.2f, QuestNamePos[num]);
+        transform.GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector2(101.9f, QuestNamePos[num]);
+        
+        if (num == 0)
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
+            transform.GetChild(3).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+            transform.GetChild(3).gameObject.SetActive(true);
+        }
 
+        QuestManager.instance.UpdateQuestUI();
+    }    
 }
--- Project_Spirit/Assets/Scripts/Quest/QuestManager.cs
+++ Project_Spirit/Assets/Scripts/Quest/QuestManager.cs
@@ -3,42 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 public class QuestManager : MonoBehaviour
-{   
+{
+    public static QuestManager instance = null;
+
     public GameObject QuestPrefab;
-    public Transform QuestUI_Transform;
-    public List<GameObject> QuestList;
-    // 퀘스트 창 생성 (기능 부여는 eventHandler로 해보자)
+    public Transform QuestUI_Transform;        
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
     public void InstantiateQuest(string name, string explain, string progress)
     {
         GameObject clone = Instantiate(QuestPrefab, QuestUI_Transform);
-        clone.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = name;
-        clone.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = explain;
-        clone.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = progress;        
-        QuestList.Add(clone);        
-        clone = null;
-    }
+        Quest quest = clone.GetComponent<Quest>();
+        quest.SetQuest(0, name, explain, progress, 0, 10, 30, 10);                        
 
-    public void ExpandQuest()
+        UpdateQuestUI();
+    }        
+       
+    public void UpdateQuestUI()
     {
-
+        float y_pos = 0;
+        for (int i = 0; i < QuestUI_Transform.childCount; i++)
+        {
+            RectTransform childRect = QuestUI_Transform.GetChild(i).GetComponent<RectTransform>();
+            float offset = childRect.sizeDelta.y / 2;
+            y_pos -= offset;
+            childRect.anchoredPosition = new Vector2(0, y_pos);
+            y_pos -= offset;
+        }
     }
-    public void ClearQuest(int idx)
-    {        
-        // 보상 지급 로직
 
-        //
-        Destroy(QuestList[idx]);
-        QuestList.RemoveAt(idx);
+
+    // For Debug.
+    public void TempInstantiateQuest()
+    {
+        GameObject clone = Instantiate(QuestPrefab, QuestUI_Transform);
+        Quest quest = clone.GetComponent<Quest>();
+        quest.SetQuest(0, "Quest1", "Quest1 Body", "Quest1 ConditionText", 0, 10, 10, 10);
+        
+        clone = null;
+
+        UpdateQuestUI();
     }
-
     // For Debug
     public void GainItem(int targetNum, int count)
     {
         // targetNum의 아이템을 count개 획득했다.        
-        Stack<int> clearIndex = new Stack<int>(); 
-        for(int i = 0; i < QuestList.Count; i++)
+        Stack<int> clearIndex = new Stack<int>();
+        for (int i = 0; i < QuestUI_Transform.childCount; i++)
         {
-            Quest quest = QuestList[i].GetComponent<Quest>();
+            Quest quest = QuestUI_Transform.GetChild(i).GetComponent<Quest>();
             if (quest.ConditionTarget == targetNum)
             {
                 quest.CurrentConditionAchieve += count;
@@ -49,20 +69,25 @@ public class QuestManager : MonoBehaviour
 
         while (clearIndex.Count != 0)
         {
-            ClearQuest(clearIndex.Pop());
+            int index = clearIndex.Pop();            
+            QuestUI_Transform.transform.GetChild(index).GetComponent<Quest>().ClearQuest();
         }
         UpdateQuestUI();
     }
-    
-    public void UpdateQuestUI()
-    {        
+
+    public void GainTree(int count)
+    {
+        GainItem(0, count);
     }
 
-    Quest quest1 = new Quest(0, "Quest1 Name", "Quest1 Body", "Quest1 ConditionText", 0, 10, 60, 10);    
-    Quest quest2 = new Quest(0, "Quest2 Name", "Quest2 Body", "Quest2 ConditionText", 0, 10, 60, 10);
+    // For Debug
     public void Start()
     {
-        quest1.SetCondition(0, 0, 5f);
-        quest2.SetCondition(1, 1, 10f);
-    }    
+        InstantiateQuest("Quest2", "Quest2 Body", "Quest2 Tree 10");
+    }
+
+    public void Update()
+    {
+        UpdateQuestUI();
+    }
 }
